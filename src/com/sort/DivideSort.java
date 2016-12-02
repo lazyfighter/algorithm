a521a9b3210688b5ca5e694cf698d1eb5d965711
LiPing <178116060@qq.com>
2016-12-02 16:09:01 +0800

    分治算法

--- src/com/sort/DivideSort.java
+++ src/com/sort/DivideSort.java
@@ -2,6 +2,26 @@ package com.sort;
 
 /**
  * Created by 李平 on 2016/12/2.
+ * --------------分治排序----------
+ *
+ * 思想：
+ *      分而治之，将原问题分解为若干个子问题，然后进行子问题求解
+ *      将子问题的解进行合并
+ *
+ *
+ *--------------------------------
  */
 public class DivideSort {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{3,2,6,4,1,20,-15,-321,23,51};
+        sort(array);
+    }
+
+    private static void sort(int[] array) {
+
+
+
+    }
+
 }
