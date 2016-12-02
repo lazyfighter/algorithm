90bf3f6e978ff0246188be34ff5a9d83e2d0b8ab
LiPing <178116060@qq.com>
2016-12-02 11:22:22 +0800

    新增插入排序

---
+++ src/com/sort/InsertSort.java
@@ -0,0 +1,22 @@
+package com.sort;
+
+/**
+ * Created by 李平 on 2016/12/2.
+ *  -------------插入排序--------------
+ */
+
+
+public class InsertSort {
+
+
+    public int[] sort(int[] array){
+
+        if (array == null || array.length <= 0) return null;
+
+        for (int i =0;i<array.length;i++){
+
+        }
+        return array;
+    }
+
+}
