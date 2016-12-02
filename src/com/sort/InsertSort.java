2907d236affdd23be01422d684c431c44e3fa9cb
LiPing <178116060@qq.com>
2016-12-02 15:00:01 +0800

    插入排序

--- src/com/sort/InsertSort.java
+++ src/com/sort/InsertSort.java
@@ -1,5 +1,7 @@
 package com.sort;
 
+import java.util.Arrays;
+
 /**
  * Created by 李平 on 2016/12/2.
  *  -------------插入排序--------------
@@ -9,18 +11,20 @@ package com.sort;
 public class InsertSort {
 
 
-    public int[] sort(int[] array){
+    public static void sort(int[] array){
 
-        if (array == null || array.length <= 0) return null;
+        if (array == null || array.length<=0) System.out.println("数组为空，请输入正确数组");
 
-        for (int i =0;i<array.length;i++){
+        for (int i =1;i<array.length;i++){
+            if (array[i]<array[i-1]){
 
+            }
         }
-        return array;
     }
 
     public static void main(String[] args) {
-
+        int[] array = new int[]{3,2,6,4,1,20,15,321,23,51};
+        sort(array);
     }
 
 }
